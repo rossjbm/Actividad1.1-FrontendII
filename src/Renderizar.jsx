8950cf2cc03c742c82c2e48a66b40b05e77ba77e
rossjbm <118684575+rossjbm@users.noreply.github.com>
2024-01-28 19:32:41 -0400

    Estilos de Renderizado 2.0, Imagenes para Cartas
    estilos para posicion y tamaño

--- src/Renderizar.jsx
+++ src/Renderizar.jsx
@@ -8,31 +8,34 @@ export function Renderizar({resultado}) {
     if (!resultado || resultado.length === 0) {
         return (<p>No Se Encontraron Resultados</p>)
     } else {
-        return (<div className="p-5 bg-green-200 grid ">
+        return (<div className="p-5 bg-green-200 grid grid-cols-3 gap-5 px-10">
             {resultado.map((libro, i) => (
-                <section key={i} className="bg-beige-200 p-5 rounded-lg">
-                    <div>
-                        <p>{libro.serial}</p>
-                        <div>
-                            <button>edit</button>
-                            <button>borr</button>
+                <section key={i} className="bg-beige-200 p-5 rounded-lg flex flex-col justify-between gap-5">
+                    <div className="flex justify-between items-center">
+                        <p className="text-base">{libro.serial}</p>
+                        <div className="flex gap-3">
+                            <button className=" rounded-xl bg-green-100 p-1">edit</button>
+                            <button className=" rounded-xl bg-green-100 p-1">borr</button>
                         </div>
                     </div>
-                    <div>
-                        <div>
-                            imagen
-                            {/* va img */}
+                    <div className="flex gap-4 items-start">
+                        <div className=" bg-blue-300 w-4/12 min-h-40 flex items-center">
+                            <img src={libro.img} className="w-full"></img>
+                            {console.log(libro.img)}
                         </div>
-                        <div>{libro.generos.map(gama => (
-                            <p>{gama}</p>
-                        ))}</div>
+                        <div className="flex flex-col gap-y-2">
+                            <h2 className="text-xl">{libro.titulo}</h2>
+                            <div className="flex flex-wrap justify-center gap-x-3 gap-y-1">{libro.generos.map(g => (<p className=" bg-blue-100 p-1 text-xs">{g}</p>))}</div>
+                            <p className="text-sm">{libro.autor}</p>
+                            <p className="text-sm">{libro.editorial} <span>- {libro.ano}</span></p>
+                        </div>
                     </div>
-                    <div>
-                        <div>
-                            <p>Disponibles</p>
-                            <p>{libro.unidades}</p>
+                    <div className="flex justify-around items-center">
+                        <div className="text-center">
+                            <p className="text-sm">Disponibles</p>
+                            <p className="text-xl">{libro.unidades}</p>
                         </div>
-                        <p>{libro.precio}$</p>
+                        <p className="text-2xl">{libro.precio}$</p>
                     </div>
                 </section>
             ))}
