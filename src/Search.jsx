cf5ded1906897f7a36b8240bb7501889968f2363
Joseeee11 <115356746+Joseeee11@users.noreply.github.com>
2024-01-28 16:05:02 -0400

    icono

--- src/Search.jsx
+++ src/Search.jsx
@@ -48,8 +48,8 @@ function Search({book}) {
         <div className="w-3/5 p-4 flex justify-around gap-2 my-16 mx-auto bg-beige-100 rounded-xl shadow-xl">
             <input type="text" placeholder="Ingresar título..." value={valor} onChange={(e) => {setValor(e.target.value)}} className="rounded-xl border border-beige-800 focus:outline-0 focus:bg-white py-2 px-3 w-9/12 bg-beige-200 text-beige-800"></input>
             <div className=" w-1/5 flex justify-center">
-                <button className="hover:bg-beige-200 px-3 m-0 h-full w-full">Fil</button>
-                <button  className="hover:bg-beige-200 border-l border-beige-800 m-0 px-3 w-full h-full">Mas</button>
+                <button className="hover:bg-beige-200 px-3 m-0 h-full w-full"><span style={{fontSize:"2.5em"}} class="material-symbols-outlined">tune</span></button>
+                <button  className="hover:bg-beige-200 border-l border-beige-800 m-0 px-3 w-full h-full"><span style={{fontSize:"2.5em"}} class="material-symbols-outlined">add</span></button>
             </div>
         </div>
 
